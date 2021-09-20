66c425a34bf2fd1238af367a382fb1c94d8954ef
jasonadams1326@gmail.com <jasonadams1326@gmail.com>
2021-09-20 11:40:10 -0500

    push

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,31 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+
+  var elements = [];
+
+  var traverse = function(element) {
+
+    if (element.classList && element.classList.contains(className)) {
+      elements.push(element);
+
+    }
+
+
+    if (element.childNodes) {
+      for (var i = 0; i < element.childNodes.length; i++) {
+
+
+        traverse(element.childNodes[i]);
+
+      }
+    }
+
+  };
+
+  traverse(document.body);
+  console.log(elements);
+  return elements;
 };
+
